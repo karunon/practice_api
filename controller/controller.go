c5601a1571f763ca5f6167c5505e4125b092ca30
karunon <54615644+karunon@users.noreply.github.com>
2022-06-04 21:59:19 +0900

    create lookTaskIntoList()

--- controller/controller.go
+++ controller/controller.go
@@ -19,6 +19,12 @@ func sortList() {
 	})
 }
 
+func lookTaskIntoList(s string) int {
+	length := len(list)
+	i := sort.Search(length, func(i int) bool { return list[i].Id >= s })
+	return i
+}
+
 func GetAll(c *gin.Context) {
 	c.JSON(http.StatusAccepted, list)
 }
@@ -53,16 +59,11 @@ func Create(c *gin.Context) {
 
 func GetOne(c *gin.Context) {
 	search_id := c.Param("task_id")
+	i := lookTaskIntoList(search_id)
 
-	flag := false
-	for _, t := range list {
-		if t.Id == search_id {
-			c.JSON(http.StatusOK, t)
-			flag = true
-			return
-		}
-	}
-	if !flag {
+	if list[i].Id == search_id {
+		c.JSON(http.StatusAccepted, list[i])
+	} else {
 		c.String(http.StatusBadRequest, "cannot find")
 	}
 }
@@ -77,18 +78,12 @@ func ModifyOne(c *gin.Context) {
 		return
 	}
 
-	flag := false
-	for num, t := range list {
-		if t.Id == search_id {
-			list[num].Title = p_json.Title
-			list[num].Description = p_json.Description
-			flag = true
-			c.JSON(http.StatusAccepted, list[num])
-			return
-		}
-	}
-
-	if !flag {
+	i := lookTaskIntoList(search_id)
+	if list[i].Id == search_id {
+		list[i].Title = p_json.Title
+		list[i].Description = p_json.Description
+		c.JSON(http.StatusAccepted, list[i])
+	} else {
 		c.String(http.StatusBadRequest, "CANNOT")
 	}
 }
